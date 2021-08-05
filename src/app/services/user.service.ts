35919f4ab8b3d13aaf6eda9b326cac38e50b0621
sofiaPalavecino <sofiaor29@gmail.com>
2021-08-05 15:41:11 -0300

    15:41 sofi

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { User } from '../shared/user.interface'; 
+import { Paseador } from "../shared/paseador";
+import {Cuidador} from "../shared/cuidador.interface"
+import {mascota} from "../shared/mascota.interface"
 
 import firebase from 'firebase/app';
 
@@ -14,8 +17,25 @@ import { AuthService } from "../services/auth.service";
 })
 export class UserService {
 
-  public usuario:User;
-  public usuarios:
+  public cuidador:Cuidador;
+  public mascotas:mascota[]=[];
+  public paseador:Paseador;
 
-  constructor() { }
+  constructor(private afs: AngularFirestore,private authSvc: AuthService) {
+    /*authSvc.user$.subscribe((user) => {
+      afs.firestore.collection("paseador").where("idUsuario", "array-contains", user.uid).get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+             doc.data() is never undefined for query doc snapshots
+            let paseadorAux:Paseador = {
+              administradores:doc.data()["calificacion promedio"],
+              mail:doc.data()["email"],
+
+            this.organizaciones.push(orgAux);
+            if(user.administrando == doc.id){
+              this.organizacion = orgAux;
+            }
+        });
+      })
+    }*/
+  }
 }
---
+++ src/app/shared/cuidador.interface.ts
@@ -0,0 +1,7 @@
+export interface Cuidador{
+    calificacion_promedio: number;
+    cupo:number;
+    disponibilidad:boolean;
+    idUsuario:string;
+    precio_dia:number;
+}
