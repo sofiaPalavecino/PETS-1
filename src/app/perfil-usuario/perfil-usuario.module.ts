45fb4eb07af5702c76093636becdf0e7beebdbf0
GianFrancoMogia <gianmoggia@gmail.com>
2021-08-13 11:30:32 -0300

    pop erreglado

--- src/app/perfil-usuario/perfil-usuario.module.ts
+++ src/app/perfil-usuario/perfil-usuario.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, PopoverController } from '@ionic/angular';
 
 import { PerfilUsuarioPageRoutingModule } from './perfil-usuario-routing.module';
 
 import { PerfilUsuarioPage } from './perfil-usuario.page';
 
 import { DirectivesModule } from '../directives/directives.module';
+import { PopoverPerfilComponent } from '../components/popover-perfil/popover-perfil.component';
 
 @NgModule({
+  declarations: [PerfilUsuarioPage,PopoverPerfilComponent],
+  providers: [PopoverPerfilComponent],
   imports: [
     CommonModule,
     FormsModule,
@@ -18,6 +21,5 @@ import { DirectivesModule } from '../directives/directives.module';
     PerfilUsuarioPageRoutingModule,
     DirectivesModule
   ],
-  declarations: [PerfilUsuarioPage]
 })
 export class PerfilUsuarioPageModule {}
