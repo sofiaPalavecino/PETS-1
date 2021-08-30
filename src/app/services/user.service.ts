6644c57135f74debcd5dc5b141dfdb47362a4d82
Marco141002 <marco@ipm-4.lan>
2021-08-30 11:36:27 -0300

    probar funcion crear paseo

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -15,6 +15,7 @@ import { switchMap } from 'rxjs/operators';
 import { AuthService } from "../services/auth.service";
 import { ConfigMascotaPageModule } from '../config-mascota/config-mascota.module';
 import { identifierModuleUrl } from '@angular/compiler';
+import { Dia } from '../dia';
 
 @Injectable({
   providedIn: 'root'
@@ -28,102 +29,92 @@ export class UserService {
   public mascotas:Array<mascota>=[];
 
   constructor(private afs: AngularFirestore,private authSvc: AuthService) {
-//que sea observable
 
-
-
-      this.afs.firestore.collection("cuidador").where("idUsuario","==",authSvc.uid).get().then((querySnapshot) => {
-        if (querySnapshot.size>0)
-          this.categorias.push("Cuidador");
-          querySnapshot.forEach((doc) =>{
-            let cuidadorAux:Cuidador ={
-              calificacion_promedio:doc.data()["calificacion promedio"],
-              idUsuario:doc.data()["idUsuario"],
-              cupo:doc.data()["cupo"],
-              disponibilidad:doc.data()["disponibilidad"],
-              precio_dia:doc.data()["precio dia"],
-            }
-            this.cuidador=cuidadorAux;
-          })
-          this.afs.firestore.collection('cuidador').where("idUsuario","==",authSvc.uid).get().then((querySnapshot)=>{
-            
-            if(querySnapshot.size>0){
-              querySnapshot.forEach((doc)=>{
-                this.afs.collection('cuidador').doc(doc.id).collection('plan cuidador').get().subscribe((querySnapshot)=>{
-                  if(querySnapshot.size>0){
-                    querySnapshot.forEach((doc) =>{
-                      let planCuidadorAux:PlanCuidador ={
-                        cantidad_dias:doc.data()["cantidad dias"],
-                        costo:doc.data()["costo"],
-                        cupo:doc.data()["cupo"],
-                        disponible:doc.data()["disponible"],
-                      }
-                      this.ofertasCuidador.push(planCuidadorAux);
-                    })
-                  }
-                })
+    this.afs.firestore.collection("cuidador").where("idUsuario","==",authSvc.uid).get().then((querySnapshot) => {
+      if (querySnapshot.size>0)
+        this.categorias.push("Cuidador");
+        querySnapshot.forEach((doc) =>{
+          this.cuidador$ = this.afs.doc<Cuidador>(`cuidador/${doc.id}`).valueChanges();
+        })
+        this.afs.firestore.collection('cuidador').where("idUsuario","==",authSvc.uid).get().then((querySnapshot)=>{
+          
+          if(querySnapshot.size>0){
+            querySnapshot.forEach((doc)=>{
+              this.afs.collection('cuidador').doc(doc.id).collection('plan cuidador').get().subscribe((querySnapshot)=>{
+                if(querySnapshot.size>0){
+                  querySnapshot.forEach((doc) =>{
+                    let planCuidadorAux:PlanCuidador ={
+                      cantidad_dias:doc.data()["cantidad dias"],
+                      costo:doc.data()["costo"],
+                      cupo:doc.data()["cupo"],
+                      disponible:doc.data()["disponible"],
+                    }
+                    this.ofertasCuidador.push(planCuidadorAux);
+                  })
+                }
               })
-            }
-          })
-      }).catch((error)=>{
-        console.log("Error getting documents: ", error);
-      })
+            })
+          }
+        })
+    }).catch((error)=>{
+      console.log("Error getting documents: ", error);
+    })
 
-      this.afs.firestore.collection("paseador").where("idUsuario","==",authSvc.uid).get().then((querySnapshot) => {
-        if (querySnapshot.size>0){
-          this.categorias.push("Paseador","Calificaciones");
-          querySnapshot.forEach((doc) =>{
-            let paseadorAux:Paseador ={
-              calificacion_promedio:doc.data()["calificacion promedio"],
-              idUsuario:doc.data()["idUsuario"],
-            }
-            this.paseador=paseadorAux;
-          })
-          this.afs.firestore.collection('paseador').where("idUsuario","==",authSvc.uid).get().then((querySnapshot)=>{
-            
-            if(querySnapshot.size>0){
-              querySnapshot.forEach((doc)=>{
-                this.afs.collection('paseador').doc(doc.id).collection('Plan Paseador').get().subscribe((querySnapshot)=>{
-                  if(querySnapshot.size>0){
-                    querySnapshot.forEach((doc) =>{
-                      let planPaseadorAux:PlanPaseo ={
-                        costo:doc.data()["costo"],
-                        cupo:doc.data()["cupo"],
-                        dias:doc.data()["dias"],
-                        disponibilidad:doc.data()["disponibilidad"],
-                        duracion:doc.data()["duracion"],
-                        duracion_plan:doc.data()["duracion plan"],
-                        hora:doc.data()["hora"],
-                      }
-                      this.planesPaseador.push(planPaseadorAux);
-                    })
-                  }
-                })
+    this.afs.firestore.collection("paseador").where("idUsuario","==",authSvc.uid).get().then((querySnapshot) => {
+      if (querySnapshot.size>0){
+        this.categorias.push("Paseador","Calificaciones");
+        querySnapshot.forEach((doc) =>{
+          let paseadorAux:Paseador ={
+            calificacion_promedio:doc.data()["calificacion promedio"],
+            idUsuario:doc.data()["idUsuario"],
+          }
+          this.paseador=paseadorAux;
+        })
+        this.afs.firestore.collection('paseador').where("idUsuario","==",authSvc.uid).get().then((querySnapshot)=>{
+          
+          if(querySnapshot.size>0){
+            querySnapshot.forEach((doc)=>{
+              this.afs.collection('paseador').doc(doc.id).collection('Plan Paseador').get().subscribe((querySnapshot)=>{
+                if(querySnapshot.size>0){
+                  querySnapshot.forEach((doc) =>{
+                    let planPaseadorAux:PlanPaseo ={
+                      costo:doc.data()["costo"],
+                      cupo:doc.data()["cupo"],
+                      dias:doc.data()["dias"],
+                      disponibilidad:doc.data()["disponibilidad"],
+                      duracion:doc.data()["duracion"],
+                      duracion_plan:doc.data()["duracion plan"],
+                      hora:doc.data()["hora"],
+                    }
+                    this.planesPaseador.push(planPaseadorAux);
+                  })
+                }
               })
-            }
-          })
-        }
-      }).catch((error)=>{
-        console.log("Error getting documents: ", error);
-      })
+            })
+          }
+        })
+      }
+    }).catch((error)=>{
+      console.log("Error getting documents: ", error);
+    })
 
-      
+    
 
-      this.afs.collection('users').doc(authSvc.uid).collection('mascota').get().subscribe((querySnapshot)=>{
-        if(querySnapshot.size>0){
-          this.categorias.push("Mascotas");
-          querySnapshot.forEach((doc) =>{
-            let mascotaAux:mascota ={
-              nombre:doc.data()["nombre"],
-            }
-            this.mascotas.push(mascotaAux);
-          })
-        }
-      });
+    this.afs.collection('users').doc(authSvc.uid).collection('mascota').get().subscribe((querySnapshot)=>{
+      if(querySnapshot.size>0){
+        this.categorias.push("Mascotas");
+        querySnapshot.forEach((doc) =>{
+          let mascotaAux:mascota ={
+            nombre:doc.data()["nombre"],
+          }
+          this.mascotas.push(mascotaAux);
+        })
+      }
+    });
   }
 
   
-  async crearNuevoPaseo(costo:number,cupo:number,plazo:string,cantDiasPaseo:number,disponibilidad:boolean,estado:string,diasDisponibles:Array<Dia>){
+  async crearNuevoPaseo(costoA:number,cupoA:number,plazoA:string,cantDiasPaseoA:number,disponibilidadA:boolean,estadoA:string,diasDisponiblesA:Array<Dia>){
     
     if(!this.paseador){ //si paseador=false, no existe documento de paseador para el usuario
       
@@ -133,7 +124,15 @@ export class UserService {
 
     }
     
-    const creoPlan = await this.afs.collection('paseador').doc().collection('Plan_Paseo').add()
+    const creoPlan = await this.afs.collection('paseador').doc().collection('Plan_Paseo').add({
+      costo:costoA,
+      cupo:cupoA,
+      plazo:plazoA,
+      cantDiasPaseo:cantDiasPaseoA,
+      disponibilidad:disponibilidadA,
+      estado:estadoA,
+      diasDisponibles:diasDisponiblesA
+    });
 
     
   }
