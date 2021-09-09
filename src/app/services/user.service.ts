67f7b9cf670a18ef4cf1d97e5720a775aa26394b
sofiaPalavecino <sofiaor29@gmail.com>
2021-09-09 16:35:55 -0300

    merge

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -46,7 +46,6 @@ export class UserService {
     
   }
 
-<<<<<<< HEAD
   async formarPerfil(id:any):Promise<Map<any,any>>{ //formar perfil para el listado de usuarios
     var funciones:Map<any,any>=new Map();
     let categorias:Array<string>=[];
@@ -100,48 +99,6 @@ export class UserService {
     }).catch((error)=>{
       console.log("Error getting documents: ", error);
     })
-=======
-      this.afs.firestore.collection("cuidador").where("idUsuario","==",authSvc.uid).get().then((querySnapshot) => {
-        if (querySnapshot.size>0)
-          this.categorias.push("Cuidador");
-          querySnapshot.forEach((doc) =>{
-            let cuidadorAux:Cuidador ={
-              calificacion_promedio:doc.data()["calificacion promedio"],
-              idUsuario:doc.data()["idUsuario"],
-              cupo:doc.data()["cupo"],
-              disponibilidad:doc.data()["disponibilidad"],
-              precio_dia:doc.data()["precio dia"],
-            }
-            this.cuidador=cuidadorAux;
-          })
-      }).catch((error)=>{
-        console.log("Error getting documents: ", error);
-      })
-
-      this.afs.firestore.collection("paseador").where("idUsuario","==",authSvc.uid).get().then((querySnapshot) => {
-        if (querySnapshot.size>0){
-          this.categorias.push("Paseador","Calificaciones");
-          querySnapshot.forEach((doc) =>{
-            let paseadorAux:Paseador ={
-              calificacion_promedio:doc.data()["calificacion promedio"],
-              idUsuario:doc.data()["idUsuario"],
-            }
-            console.log(doc.data())
-            this.paseador=paseadorAux;
-          })
-          this.afs.collection('paseador').doc(authSvc.uid).collection('Plan Paseador').get().subscribe((querySnapshot)=>{
-            if(querySnapshot.size>0){
-              querySnapshot.forEach((doc)=>{
-                
-              })
-            }
-          })
-        }
-          
-      }).catch((error)=>{
-        console.log("Error getting documents: ", error);
-      })
->>>>>>> 558f584528a94b5f30dac62d24727acede81c603
 
     await this.afs.collection('users').doc(id).collection('mascota').get().subscribe((querySnapshot)=>{
       if(querySnapshot.size>0){
