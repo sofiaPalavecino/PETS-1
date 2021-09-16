76a84a17326ba17f1d79e8d9e2cc5867a6a303c2
sofiaPalavecino <sofiaor29@gmail.com>
2021-09-16 15:52:58 -0300

    a

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -12,7 +12,6 @@ import { UserService } from '../services/user.service';
 export class PerfilPage implements OnInit {
 
   public usuarios:Array<userProfile> = []
-  public funciones:any;
   public id:any=0;
 
   constructor(public ActivatesToute: ActivatedRoute,public afs:AngularFirestore, public userServ:UserService) { }
@@ -20,26 +19,6 @@ export class PerfilPage implements OnInit {
   async ngOnInit() {
 
     
-    
-    this.afs.firestore.collection("users").where("uid","==",this.ActivatesToute.snapshot.paramMap.get("id")).get().then((querySnapshot)=>{
-      if(querySnapshot.size>0){
-        querySnapshot.forEach((doc) =>{
-          let userAux:userProfile = {
-            nombre: doc.data()["nombre"],
-            apellido: doc.data()["apellido"],
-            uid: doc.data()["uid"],
-            email: doc.data()["email"],
-            emailVerified: doc.data()["emailVerified"],
-            nacimiento: doc.data()["nacimiento"],
-            administrando:doc.data()["administrando"],
-            DNI:doc.data()["DNI"],
-            foto:doc.data()["foto"],
-            barrio:doc.data()["barrio"]
-          }
-          this.usuarios.push(userAux);
-        })
-      } 
-    })
   }
 
 }
