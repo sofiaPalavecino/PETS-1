9c4ab1c7705dbe88c4416c2c0b7357e5d1a3b729
NahuelLeto <alumno21.leto.nahuel@ipm.edu.ar>
2021-10-05 13:11:26 -0300

    Entrar a publicaciones

--- src/app/publicacion/publicacion.page.ts
+++ src/app/publicacion/publicacion.page.ts
@@ -1,11 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Observable } from 'PETS/node_modules/rxjs';
+import { Observable } from 'rxjs';
 import { PubliService } from '../services/publi.service';
 import { variable } from '@angular/compiler/src/output/output_ast';
 import { MenuComponent } from '../components/menu/menu.component';
 import { OrganizacionService } from "../services/organizacion.service";
 import { Publicacion } from '../shared/publicacion';
-import { hostViewClassName } from 'PETS/node_modules/@angular/compiler';
+//import { hostViewClassName } from 'PETS/node_modules/@angular/compiler';
 import { ActivatedRoute } from '@angular/router';
 
 
@@ -31,8 +31,14 @@ export class PublicacionPage implements OnInit {
     this.org.actualizarOrganizacion(this.org.oid);
   }
 
-  setPublicacion(){
-    //this.publiServ.getIDPublicacion(this.publiServ);
+  mostrarTexto(id:string){
+    let vista=document.getElementById(id).style.display;
+	if (vista=='none')
+		vista='block';
+	else
+		vista='none';
+
+	document.getElementById(id).style.display = vista;
   }
 
 }
