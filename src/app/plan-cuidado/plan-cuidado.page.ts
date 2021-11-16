b8d53941032d8ced323e387e5672e1c42a6213a8
sofiaPalavecino <sofiaor29@gmail.com>
2021-11-16 17:30:33 -0300

    prettier

--- src/app/plan-cuidado/plan-cuidado.page.ts
+++ src/app/plan-cuidado/plan-cuidado.page.ts
@@ -1,40 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../services/user.service';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../services/user.service";
 
 import { AlertController } from "@ionic/angular";
 import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-plan-cuidado',
-  templateUrl: './plan-cuidado.page.html',
-  styleUrls: ['./plan-cuidado.page.scss'],
+  selector: "app-plan-cuidado",
+  templateUrl: "./plan-cuidado.page.html",
+  styleUrls: ["./plan-cuidado.page.scss"],
 })
 export class PlanCuidadoPage implements OnInit {
+  costo: number;
+  maximoMascotas: number;
 
-  costo:number;
-  maximoMascotas:number;
-
-  constructor(private userServ: UserService,public alertController: AlertController,private router: Router) { 
-    
-    
-  }
+  constructor(
+    private userServ: UserService,
+    public alertController: AlertController,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.costo=0;
-    this.maximoMascotas=0;
+    this.costo = 0;
+    this.maximoMascotas = 0;
   }
 
-  async crearCuidado(){
-    console.log(this.costo,this.maximoMascotas);
-    
-    if(this.costo==0 || this.maximoMascotas==0){
-      this.presentAlert("Datos incompletos","Por favor, ingresar todos los datos")
-    }else{
-      this.userServ.crearNuevoCuidado(this.costo,this.maximoMascotas);
+  async crearCuidado() {
+    console.log(this.costo, this.maximoMascotas);
+
+    if (this.costo == 0 || this.maximoMascotas == 0) {
+      this.presentAlert(
+        "Datos incompletos",
+        "Por favor, ingresar todos los datos"
+      );
+    } else {
+      this.userServ.crearNuevoCuidado(this.costo, this.maximoMascotas);
       this.router.navigate(["/perfil-usuario"]);
     }
-
-    
   }
 
   async presentAlert(subtitulo: string, mensaje: string) {
@@ -47,5 +48,4 @@ export class PlanCuidadoPage implements OnInit {
 
     await alert.present();
   }
-
 }
